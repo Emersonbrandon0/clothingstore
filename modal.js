ea71c4f17012155f339b291dde4b98f49abb7118
Brandon Emerson <emersonbrandon0@gmail.com>
2017-03-28 13:08:44 -0400

    First commit

---
+++ modal.js
@@ -0,0 +1,16 @@
+<script>
+var modal = document.getElementById('openModal');
+var span = document.getElementsByClassName("close")[0];
+var submitButton = document.getElementById("button");
+var destination = document.getElementById("destination");
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+submitButton.onclick = function() {
+	destination.innerHTML="Thank you for submitting");
+
+}
+
+
+</script>
